ca8dc0147614ca6f09384d719dccc49180f0a268
Robert Bishop <rbishop2006@gmail.com>
2020-03-20 16:15:48 -0700

    #23 worked on database

---
+++ server/db.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql")
+const config = require("config")
+const pool = mysql.createPool({
+  connectionLimit: 25,
+  host: config.get("db.host"),
+  user: config.get("db.user"),
+  password: config.get("db.password"),
+  database: config.get("db.database")
+})
+module.exports = pool
