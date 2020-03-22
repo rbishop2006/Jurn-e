a91784e363ac18bd40ebd66c42c764639230aa65
Michael Quarne <mikedq1225@gmail.com>
2020-03-22 14:57:52 -0700

    created header, no functionality at this point closes #10

---
+++ client/src/components/jurn(e)/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Header } from "semantic-ui-react"
+
+export default props => {
+  return (
+    <header>
+      <Header inverted size="large">
+        Jurn(e)
+      </Header>
+    </header>
+  )
+}
