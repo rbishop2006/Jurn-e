52006140b58613ea737f1e89dc33da7d03023005
Michael Quarne <mikedq1225@gmail.com>
2020-03-22 13:52:10 -0700

    #9 created framework for aside

--- client/src/components/jurn(e)/Dashboard.js
+++ client/src/components/jurn(e)/Dashboard.js
@@ -1,19 +1,15 @@
 import React from "react"
-import { Button } from "semantic-ui-react"
+// import { Button } from "semantic-ui-react"
 import { useAuth } from "react-auth"
 // import RoomMain from "./RoomMain"
-// import RoomAside from "./RoomAside"
-// import "../../styles/chatroom.scss"
+import Aside from "./Aside"
+import "../../styles/dashboard.scss"
 
 export default props => {
-  const { signout } = useAuth()
-
   return (
     <div className="grid">
-      <h1>hello world</h1>
-      <Button onClick={e => signout()}>Log out</Button>
-      {/* <RoomAside />
-      <RoomMain room={props.match.params.page} /> */}
+      <Aside />
+      {/* <RoomMain room={props.match.params.page} /> */}
     </div>
   )
 }
