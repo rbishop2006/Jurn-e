6fb5eb0149428e6db412c8ea045a591811512cfc
Robert Bishop <rbishop2006@gmail.com>
2020-03-24 15:32:56 -0700

    pushing reminder

--- server/routes/get.js
+++ server/routes/get.js
@@ -4,6 +4,7 @@ const conn = require("../db")
 const decode = require("jsonwebtoken").decode
 router.get("/dashboard", (req, res, next) => {
   const profile = decode(req.headers.authorization.substring(7))
+
   const dashResults = {}
   const sql = `SELECT
 view.jname, view.loc_name, view.user_id, user.fname, user.lname, user.fam_id, family.fam_name, user.email, user.cell_phone
@@ -17,14 +18,19 @@ FROM
     LEFT JOIN family ON family.id = user.fam_id`
   conn.query(sql, (err, results, fields) => {
     dashResults.jurn = results
-    const email = profile.email
 
+    const email = profile.email
     const sql2 = `SELECT user.id, user.email, user.fname, user.lname, user.fam_id, user.cell_phone
       FROM user
       WHERE user.email = ?`
     conn.query(sql2, [email], (err2, results2, fields2) => {
       dashResults.user = results2[0]
-      res.json({ dashboard: dashResults })
+
+      const sql3 = `SELECT * FROM reminder`
+      conn.query(sql3, (err3, results3, fields3) => {
+        dashResults.reminders = results3
+        res.json({ dashboard: dashResults })
+      })
     })
   })
 })
