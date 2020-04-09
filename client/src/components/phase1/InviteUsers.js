71b08f82b2eaa152cea98a75e18913c0c8dd4423
Robert Bishop <rbishop2006@gmail.com>
2020-04-08 17:37:12 -0700

    128, add validator and backend check on invite for phase1, closes #128

--- client/src/components/phase1/InviteUsers.js
+++ client/src/components/phase1/InviteUsers.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react"
 import { Form, Button, Radio, List, Checkbox, Icon } from "semantic-ui-react"
+import validator from "validator"
 import { useInvited } from "../../hooks"
 import "../../styles/phase1/inviteUsers.scss"
 
 export default props => {
+  //trying to add validator
+
   const {
     pending,
     accepted,
@@ -14,14 +17,45 @@ export default props => {
 
   const jurn_id = props.match.params.jurn_id
   const [firstName, setFirstName] = useState("")
+  const [firstNameError, setFirstNameError] = useState("")
   const [lastName, setLastName] = useState("")
+  const [lastNameError, setLastNameError] = useState("")
+  const [error, setError] = useState(false)
+
+  //trying to set up error handle
 
   function handleInvite(e) {
     e.preventDefault()
-    sendInvite(firstName, lastName, jurn_id)
-    setFirstName("")
-    setLastName("")
-    updateInvited(jurn_id)
+
+    let valid = true
+
+    if (validator.isEmpty(firstName)) {
+      valid = false
+      setFirstNameError("Cannot be blank, must contain a first name")
+    } else {
+      setFirstNameError("")
+    }
+    if (validator.isEmpty(lastName)) {
+      valid = false
+      setLastNameError("Cannot be blank, must contain a first name")
+    } else {
+      setLastNameError("")
+    }
+    if (valid) {
+      sendInvite(firstName, lastName, jurn_id)
+        .then(e => {
+          setFirstName("")
+          setLastName("")
+          updateInvited(jurn_id)
+        })
+
+        .catch(e => {
+          setError(true)
+          setFirstName("")
+          setLastName("")
+          updateInvited(jurn_id)
+        })
+    }
   }
 
   useEffect(() => {
@@ -36,17 +70,37 @@ export default props => {
         </h3>
         <Form.Group className="inviteSect">
           <Form.Input
+            error={
+              error
+                ? { content: "Invitee not found", pointing: "below" }
+                : false
+            }
             fluid
-            label="First Name"
+            label={
+              firstNameError
+                ? "Cannot be blank, must contain a first name"
+                : "First Name"
+            }
             placeholder="ex. Mary"
             value={firstName}
+            className={firstNameError ? "error" : ""}
             onChange={e => setFirstName(e.target.value)}
           />
           <Form.Input
+            error={
+              error
+                ? { content: "Invitee not found", pointing: "below" }
+                : false
+            }
             fluid
-            label="Last Name"
+            label={
+              lastNameError
+                ? "Cannot be blank, must contain a last name"
+                : "Last Name"
+            }
             placeholder="ex. Smith"
             value={lastName}
+            className={lastNameError ? "error" : ""}
             onChange={e => setLastName(e.target.value)}
           />
         </Form.Group>
