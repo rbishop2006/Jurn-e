0862b166d60196a8ee93b1d26cc1f04e315d357b
Michael Quarne <mikedq1225@gmail.com>
2020-05-17 13:43:47 -0700

    changed main card view to flex

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,20 +8,20 @@ const Login = React.lazy(() => import("./auth/Login"))
 const Register = React.lazy(() => import("./auth/Register"))
 const Dashboard = React.lazy(() => import("./dashboard/Dashboard"))
 
-export default props => {
-  return (
-    <AuthProvider redirectUrl="/Jurne">
-      <Router>
-        <div className="app">
-          <Suspense fallback={<div>Loading...</div>}>
-            <Route exact path="/Jurne" component={Jurne} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route exact path="/" render={() => <Redirect to="/Jurne" />} />
-            <AuthRoute path="/Jurne/:page" component={Dashboard} />
-          </Suspense>
-        </div>
-      </Router>
-    </AuthProvider>
-  )
+export default (props) => {
+	return (
+		<AuthProvider redirectUrl="/Jurne">
+			<Router>
+				<div className="app">
+					<Suspense fallback={<div>Loading...</div>}>
+						<Route exact path="/Jurne" component={Jurne} />
+						<Route path="/login" component={Login} />
+						<Route path="/register" component={Register} />
+						<Route exact path="/" render={() => <Redirect to="/Jurne" />} />
+						<AuthRoute path="/Jurne/:page" component={Dashboard} />
+					</Suspense>
+				</div>
+			</Router>
+		</AuthProvider>
+	)
 }
