71844e1ea450ad25af091b372f784d3550797b0e
Michael Quarne <mikedq1225@gmail.com>
2020-07-24 16:40:52 -0700

    media query on landing page

--- client/src/components/Jurne.js
+++ client/src/components/Jurne.js
@@ -3,33 +3,33 @@ import { Link } from "react-router-dom"
 import { Button } from "semantic-ui-react"
 import "../styles/frontPage.scss"
 
-export default props => {
-  return (
-    <div className="frontWrapper">
-      <div className="frontHeader">
-        <Link to="/login" className="logIn">
-          Log in
-        </Link>
-        <Link to="/register">
-          <Button type="button" className="signUp">
-            Sign up for <em>free</em>
-          </Button>
-        </Link>
-      </div>
-      <div className="frontMain">
-        <div className="tagline">
-          <h1>Traveling with family or friends?</h1>
-          <p>
-            The first step in any journey is planning. Jurn(<em>ease</em>) makes
-            that a collaborative process by allowing you to build your trip
-            together through sharing location ideas, places to stay, activity
-            suggestions, discussions, an itinerary, photos, and expenses.
-          </p>
-          <h3>
-            Keep and Cherish Your Travel Experiences with Jurn(<em>ease</em>).
-          </h3>
-        </div>
-      </div>
-    </div>
-  )
+export default (props) => {
+	return (
+		<div id="frontWrapper">
+			<div className="frontHeader">
+				<Link to="/login" className="logIn">
+					Log in
+				</Link>
+				<Link to="/register">
+					<Button type="button" className="signUp">
+						Sign up for <em>free</em>
+					</Button>
+				</Link>
+			</div>
+			<div className="frontMain">
+				<div className="tagline">
+					<h1>Traveling with family or friends?</h1>
+					<p>
+						The first step in any journey is planning. Jurn(<em>ease</em>) makes
+						that a collaborative process by allowing you to build your trip
+						together through sharing location ideas, places to stay, activity
+						suggestions, discussions, an itinerary, photos, and expenses.
+					</p>
+					<h3>
+						Keep and Cherish Your Travel Experiences with Jurn(<em>ease</em>).
+					</h3>
+				</div>
+			</div>
+		</div>
+	)
 }
