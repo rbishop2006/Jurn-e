e4fa956f31c45d1d72abdb8521c1d604a5fa1e73
Michael Quarne <mikedq1225@gmail.com>
2020-07-27 12:27:22 -0700

    refactored login pages again

--- client/src/components/dashboard/Main.js
+++ client/src/components/dashboard/Main.js
@@ -2,56 +2,57 @@ import React, { useEffect } from "react"
 import { Card, Image, Button, Icon } from "semantic-ui-react"
 import { useMain } from "../../hooks"
 import { Link } from "react-router-dom"
+import "../../styles/dashboard/Main.scss"
 import moment from "moment"
 
 export default (props) => {
-  const { jurns, get } = useMain()
+	const { jurns, get } = useMain()
 
-  useEffect(() => {
-    get()
-  }, [])
+	useEffect(() => {
+		get()
+	}, [])
 
-  return (
-    <main>
-      <div className="dashMain">
-        {/* This is where we display info about each Jurn in the database */}
-        <div className="cardDiv">
-          {jurns.map((jurn, i) => (
-            <Card className="cards" key={"jurn" + i} centered>
-              <Image
-                className="photos"
-                src={jurn.photo}
-                alt="Jurn(ease) trip photo"
-              />
-              <Card.Content>
-                <Card.Header className="nameAndEdit">{jurn.jname}</Card.Header>
-                <Card.Meta id="location">{jurn.location}</Card.Meta>
-                <Card.Meta>
-                  {jurn.start_date &&
-                    moment(jurn.start_date).format("MMM Do, YYYY") + " - "}
-                  {jurn.end_date &&
-                    moment(jurn.end_date).format("MMM Do, YYYY")}
-                </Card.Meta>
-                <Card.Meta>{`People going: ` + jurn.going_count}</Card.Meta>
-                <Card.Meta>{`Pending invites: ` + jurn.pend_count}</Card.Meta>
-                <Card.Meta>{`Activities planned: ` + jurn.act_count}</Card.Meta>
+	return (
+		<section>
+			<div className="dashMain">
+				{/* This is where we display info about each Jurn in the database */}
+				<div className="cardDiv">
+					{jurns.map((jurn, i) => (
+						<Card className="cards" key={"jurn" + i} centered>
+							<Image
+								className="photos"
+								src={jurn.photo}
+								alt="Jurn(ease) trip photo"
+							/>
+							<Card.Content>
+								<Card.Header className="nameAndEdit">{jurn.jname}</Card.Header>
+								<Card.Meta id="location">{jurn.location}</Card.Meta>
+								<Card.Meta>
+									{jurn.start_date &&
+										moment(jurn.start_date).format("MMM Do, YYYY") + " - "}
+									{jurn.end_date &&
+										moment(jurn.end_date).format("MMM Do, YYYY")}
+								</Card.Meta>
+								<Card.Meta>{`People going: ` + jurn.going_count}</Card.Meta>
+								<Card.Meta>{`Pending invites: ` + jurn.pend_count}</Card.Meta>
+								<Card.Meta>{`Activities planned: ` + jurn.act_count}</Card.Meta>
 
-                <Card.Meta>{`My Reminders Left: ` + jurn.rem_count}</Card.Meta>
-                <Link
-                  className="p1Edit"
-                  to={"/Jurne/dashboard/" + jurn.jurn_id}
-                >
-                  <Button type="button">
-                    <span>
-                      J(<em>e</em>) <Icon name="pencil" />
-                    </span>
-                  </Button>
-                </Link>
-              </Card.Content>
-            </Card>
-          ))}
-        </div>
-      </div>
-    </main>
-  )
+								<Card.Meta>{`My Reminders Left: ` + jurn.rem_count}</Card.Meta>
+								<Link
+									className="p1Edit"
+									to={"/Jurne/dashboard/" + jurn.jurn_id}
+								>
+									<Button type="button">
+										<span>
+											J(<em>e</em>) <Icon name="pencil" />
+										</span>
+									</Button>
+								</Link>
+							</Card.Content>
+						</Card>
+					))}
+				</div>
+			</div>
+		</section>
+	)
 }
